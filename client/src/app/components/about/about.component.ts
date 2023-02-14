50b0a752b89355c05852b7343ac642b2828d71b9
Nhat Anh Pham <nhatap@uci.edu>
2023-02-13 18:08:44 -0800

    updated about me page w/ correct angular binding

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -14,23 +14,24 @@ export class AboutComponent implements OnInit {
 
   //TODO: inject the Spotify service
   constructor(private service: SpotifyService) { }
-
+  
   ngOnInit() {
-    let me =this.service.aboutMe();
-    document.getElementsByTagName("button")[0].addEventListener("click", function(){
-      me.then((data)=>{
-      document.getElementsByTagName("h3")[0].innerText = "Logged in user: "+data.name;
-      document.getElementById("mePic").setAttribute("src", data.imageURL);
-      document.getElementById("openProfile").setAttribute("href", data.spotifyProfile)
-      //console.log(document.getElementsByTagName("a")[0])
-      })
-      .catch((error: any) => console.log(error));
-    })
-    
+    this.name = "???";
+    this.getAboutMe();
   }
 
 
   /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
   In that function, update the name, profile_pic, and profile_link fields */
-
+  getAboutMe() {
+    let aboutMe = this.service.aboutMe();
+    let _self = this;
+    document.getElementsByTagName("button")[0].addEventListener("click", function() {
+      aboutMe.then((data) => {
+        _self.name = data.name;
+        _self.profile_pic = data.imageURL;
+        _self.profile_link = data.spotifyProfile;
+      }).catch((error: any) => console.log(error));
+    });
+  }
 }
